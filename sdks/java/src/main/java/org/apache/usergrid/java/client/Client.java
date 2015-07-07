a4f57bae3ce45ffc3889735a29f785d99042d10b
ayesha12 <ayesha.amrin@gmail.com>
2015-07-07 12:49:36 -0700

    adding sdk changes for Connection . Created connection.java to handle all conection related functionalities.

--- sdks/java/src/main/java/org/apache/usergrid/java/client/Client.java
+++ sdks/java/src/main/java/org/apache/usergrid/java/client/Client.java
@@ -32,8 +32,8 @@ import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import org.apache.usergrid.java.client.entities.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpEntity;
@@ -43,11 +43,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
-import org.apache.usergrid.java.client.entities.Activity;
-import org.apache.usergrid.java.client.entities.Device;
-import org.apache.usergrid.java.client.entities.Entity;
-import org.apache.usergrid.java.client.entities.Group;
-import org.apache.usergrid.java.client.entities.User;
 import org.apache.usergrid.java.client.response.ApiResponse;
 
 /**
@@ -358,7 +353,7 @@ public class Client {
     return response;
   }
 
-  protected void assertValidApplicationId() {
+  public void assertValidApplicationId() {
     if (isEmpty(applicationId)) {
       throw new IllegalArgumentException("No application id specified");
     }
@@ -1002,6 +997,11 @@ public class Client {
         connectedEntityId);
   }
 
+  public ApiResponse queryEntity(String type,String id) {
+    return apiRequest(HttpMethod.GET, null, null, organizationId, applicationId,
+            type,id);
+  }
+
   /**
    * Query the connected entities.
    *
@@ -1056,12 +1056,18 @@ public class Client {
 
   }
 
-  public ApiResponse connectEntities(Entity pet, Entity owner, String connetionName) {
 
-    // check for UUIDs
+  public ApiResponse connectEntities(Entity sourceVertex, Entity TargetVertex, String connetionName) {
 
-    return this.connectEntities(pet.getType(), pet.getUuid().toString(), connetionName, owner.getUuid().toString());
+    return this.connectEntities(sourceVertex.getType(), sourceVertex.getUuid().toString(), connetionName, TargetVertex.getUuid().toString());
   }
+
+
+
+  public Entity getEntity(String s) {
+    return null ;
+  }
+
 
   public interface Query {
 
