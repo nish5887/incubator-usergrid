1d39e8b5b58cdde6582de55dd4ce1a00c7d300b8
Jeff West <jwest@apigee.com>
2015-07-22 15:05:53 -0700

    caching, WIP

--- sdks/java/src/main/java/com/apigee/sdk/ApiClient.java
+++ sdks/java/src/main/java/com/apigee/sdk/ApiClient.java
@@ -1,12 +1,13 @@
 package com.apigee.sdk;
 
+import org.apache.http.impl.client.cache.CacheConfig;
+
 /**
  * Created by ApigeeCorporation on 7/22/15.
  */
 public class ApiClient {
   private OAuthAuthenticationStrategy authenticationStrategy;
   private HttpCompliantCache cachingStrategy;
-  private CacheConfig foo;
 
   public void setAuthenticationStrategy(OAuthAuthenticationStrategy authenticationStrategy) {
     this.authenticationStrategy = authenticationStrategy;
--- sdks/java/src/main/java/com/apigee/sdk/HttpCompliantCache.java
+++ sdks/java/src/main/java/com/apigee/sdk/HttpCompliantCache.java
@@ -1,10 +1,17 @@
 package com.apigee.sdk;
 
+import org.apache.http.impl.client.cache.CacheConfig;
+
 /**
  * Created by ApigeeCorporation on 7/22/15.
  */
 public class HttpCompliantCache {
   private String name;
+  private CacheConfig cacheConfig = CacheConfig.custom()
+      .setMaxCacheEntries(1000)
+      .setMaxObjectSize(8192)
+      .build();
+
 
   public HttpCompliantCache(String name) {
     this.name = name;
